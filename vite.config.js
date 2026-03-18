f10b9633b46f96da09a06a04843d79d3aa56d13b
paulhummel787-web <paulhummel787@gmail.com>
2026-03-18 13:16:23 -0400

    Update comment for base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // The dot is CRITICAL. It tells the app "look right here for files."
+  base: './', // This tells the app to look in the current folder
 })
