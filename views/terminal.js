ca0db0527314f6257a8a9033d42c2be3a2b4103f
paulhummel787-web <paulhummel787@gmail.com>
2026-03-19 04:11:54 -0400

    Create terminal.js

---
+++ views/terminal.js
@@ -0,0 +1,8 @@
+export function renderTerminal() {
+  return `
+    <div class="glass">
+      <h2 class="text-xl mb-4">Terminal</h2>
+      <p class="opacity-70">Command system coming online.</p>
+    </div>
+  `;
+}
