6132baad7899e0b88e6054d71103262a55ff9725
paulhummel787-web <paulhummel787@gmail.com>
2026-03-18 21:03:25 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: './', // This tells the app to look in the current folder
-})
